bd32ca583843aa5904acaa96b0ee46f1c1a4326b
arya <aryasyama402@gmail.com>
2020-03-23 21:27:34 +0530

    design

--- staff/pages/tables/status.php
+++ staff/pages/tables/status.php
@@ -298,7 +298,8 @@ $se="SELECT * FROM `tbl_leave` WHERE staffid='$sid'";
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!--<img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">-->
-              <span class="hidden-xs">Profile</span>
+                <img src="logo.jpg" class="user-image" alt="User Image">
+              <span class="hidden-xs"><?php echo $fname;?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
