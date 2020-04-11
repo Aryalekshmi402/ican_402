2b56bf2b054e3b7b007a36d6583a8644c81e0a56
arya <aryasyama402@gmail.com>
2020-04-12 01:12:05 +0530

    material upload

--- student/pages/tables/download_files.php
+++ student/pages/tables/download_files.php
@@ -3,10 +3,10 @@ session_start();
 include('../../../config.php');
 $loginid = $_SESSION['loginid'];
 $syid=$_GET['sy_id'];
-$q="select * from tbl_material where syid=$syid and classid=(select course from studtbl where loginid=$loginid)";
+$q="select * from tlb_material where syid=$syid and classid=(select course from studtbl where loginid=$loginid)";
 $res=mysqli_query($con,$q);
 $myArray = array();
 while ($row = mysqli_fetch_array($res)) {
     $myArray[] = $row;
 }
-echo json_encode(['reports' => $myArray]);
+echo json_encode(['materials' => $myArray]);
