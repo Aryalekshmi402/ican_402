2375cb14126df9db8b30ce3580d6d5d725437d74
arya <aryasyama402@gmail.com>
2020-04-24 18:56:44 +0530

    cpanel modification

--- student/pages/tables/view_mark.php
+++ student/pages/tables/view_mark.php
@@ -272,9 +272,7 @@ if($l)
                             </ul>
                         </li>
                         <!-- Control Sidebar Toggle Button -->
-                        <li>
-                            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                        </li>
+
                     </ul>
                 </div>
             </nav>
@@ -1043,7 +1041,7 @@ if($l)
             <!-- Create the tabs -->
             <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
                 <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-                <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+
             </ul>
             <!-- Tab panes -->
             <div class="tab-content">
