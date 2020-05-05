3f65534586b1f1548248f116927d6e2feb898b10
arya <aryasyama402@gmail.com>
2020-05-05 11:06:28 +0530

    view timetable in staff

--- staff/menu.php
+++ staff/menu.php
@@ -85,6 +85,28 @@
                     <!--<li><a href="attendance_report.php"><i class="fa fa-circle-o"></i> Attendance Report</a></li>-->
                 </ul>
             </li>
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-files-o"></i>
+                    <span>Scheduling Details</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="/staff/pages/tables/viewschedule.php"><i class="fa fa-circle-o"></i>View Schedule</a></li>
+                    <!--<li><a href="attendance_report.php"><i class="fa fa-circle-o"></i> Attendance Report</a></li>-->
+                </ul>
+            </li>
+
+
+
+
+
+
+
+
         </ul>
     </section>
     <!-- /.sidebar -->
